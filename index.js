a28dc6f199a7c7d95ef913b5d957971a1c5a2d73
Ilya Shishkin <mostbook@mail.ru>
2022-04-06 21:47:20 +0700

    update

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
-const sequelize = require('./db');
-const models = require('./models/models');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-const router = require('./routes/index');
-const errorHandler = require('./middleware/ErrorHandlingMiddleware');
-const path = require('path');
+// const sequelize = require('./db');
+// const models = require('./models/models');
+// const cors = require('cors');
+// const fileUpload = require('express-fileupload');
+// const router = require('./routes/index');
+// const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+// const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 
 const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.static(path.resolve(__dirname, 'static')));
-app.use(fileUpload({}));
-app.use('/api', router);
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.static(path.resolve(__dirname, 'static')));
+// app.use(fileUpload({}));
+// app.use('/api', router);
+//
+// app.use(errorHandler); // Must be the last use()
 
-app.use(errorHandler); // Must be the last use()
 
+// const start = async () => {
+//     try {
+//         await sequelize.authenticate();
+//         await sequelize.sync();
+//         app.listen(PORT, () => {
+//             console.log(`Server started on port ${PORT}`);
+//         });
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+//
+// start();
 
-const start = async () => {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync();
-        app.listen(PORT, () => {
-            console.log(`Server started on port ${PORT}`);
-        });
-    } catch (e) {
-        console.log(e)
-    }
-}
+app.get('/', (req, res) => {
+    res.end('<h1>Page</h1>');
+})
 
-start();
+app.listen(PORT, () => {
+    console.log("started")
+})
