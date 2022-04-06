2f30d89a57f46ab365389fe711244b99bd86c0c5
Ilya Shishkin <mostbook@mail.ru>
2022-04-06 21:57:29 +0700

    update

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const {Sequelize} = require('sequelize');
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DATABASE_URL,
     {
+        // host: process.env.DB_HOST,
+        // port: process.env.DB_PORT,
         dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: true
+        },
     }
 );
--- index.js
+++ index.js
@@ -1,44 +1,36 @@
-// require('dotenv').config();
+require('dotenv').config();
 const express = require('express');
-// const sequelize = require('./db');
-// const models = require('./models/models');
-// const cors = require('cors');
-// const fileUpload = require('express-fileupload');
-// const router = require('./routes/index');
-// const errorHandler = require('./middleware/ErrorHandlingMiddleware');
-// const path = require('path');
+const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 
 const app = express();
-// app.use(cors());
-// app.use(express.json());
-// app.use(express.static(path.resolve(__dirname, 'static')));
-// app.use(fileUpload({}));
-// app.use('/api', router);
-//
-// app.use(errorHandler); // Must be the last use()
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+app.use('/api', router);
+
+app.use(errorHandler); // Must be the last use()
 
 
-// const start = async () => {
-//     try {
-//         await sequelize.authenticate();
-//         await sequelize.sync();
-//         app.listen(PORT, () => {
-//             console.log(`Server started on port ${PORT}`);
-//         });
-//     } catch (e) {
-//         console.log(e)
-//     }
-// }
-//
-// start();
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        });
+    } catch (e) {
+        console.log(e)
+    }
+}
 
-app.get('/', (req, res) => {
-    res.end('<h1>Page</h1>');
-})
-
-app.listen(PORT, () => {
-    console.log("started")
-})
+start();
